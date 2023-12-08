c9ed4870c1270953172dd4f1d49931497653ebc7
ShashiKumar03 <shashikumarkushwaha3@gmail.com>
2023-12-08 09:59:35 +0530

    player with max pom

--- src/server/6-player-won-maximum-pom-each-season.js
+++ src/server/6-player-won-maximum-pom-each-season.js
@@ -1,3 +1,31 @@
-function playerWonMaxPomEachSeason() {}
+const knex = require("../../knex-database-connection/knexConnection");
+
+function playerWonMaxPomEachSeason() {
+  const subquery = knex("matches as m2")
+    .select(knex.raw("count(m2.player_of_match)"))
+    .whereRaw("m2.season = m.season")
+    .groupBy("m2.player_of_match")
+    .orderByRaw("count(*) desc")
+    .limit(1);
+
+  const mainQuery = knex("matches as m")
+    .select("season", "player_of_match")
+    .count("player_of_match as pom")
+    .groupBy("season", "player_of_match")
+    .having("pom", "=", subquery)
+    .orderBy("season");
+
+  // Executeing  main query
+  return mainQuery
+    .then((results) => {
+      return results;
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      knex.destroy();
+    });
+}
 
 module.exports = playerWonMaxPomEachSeason;
--- test/testPlayerWonMaximumPomEachSeason.js
+++ test/testPlayerWonMaximumPomEachSeason.js
@@ -1,3 +1,9 @@
 const playerWonMaxPomEachSeason = require("../src/server/6-player-won-maximum-pom-each-season");
 
-playerWonMaxPomEachSeason();
+playerWonMaxPomEachSeason()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    throw err;
+  });
