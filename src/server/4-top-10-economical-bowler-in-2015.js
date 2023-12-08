fb10b6118d4dbd6e1929d0a267d844d802decf57
ShashiKumar03 <shashikumarkushwaha3@gmail.com>
2023-12-08 10:11:24 +0530

    q 4 modify

--- src/server/4-top-10-economical-bowler-in-2015.js
+++ src/server/4-top-10-economical-bowler-in-2015.js
@@ -1,35 +1,28 @@
 const knex = require("../../knex-database-connection/knexConnection");
 
 function top10EconomicalBowlerIn2015() {
-  const subquery = [];
-
-  return knex
+  const subQuery = knex
     .select("id")
     .from("matches as m")
-    .where("season", "=", "2015")
-    .then((result) => {
-      result.forEach((data) => {
-        subquery.push(data.id);
-      });
+    .where("season", "=", "2015");
 
-      return knex
-        .select(
-          "bowler",
-          knex.raw(
-            "(6 * SUM(total_runs - legbye_runs) / count(bowler)) as economy"
-          )
-        )
-        .from("deliveries as d")
-        .whereIn("match_id", subquery)
-        .groupBy("bowler")
-        .orderBy("economy", "ASC")
-        .limit(10);
-    })
+  const mainQuery = knex
+    .select(
+      "bowler",
+      knex.raw("(6 * SUM(total_runs - legbye_runs) / count(bowler)) as economy")
+    )
+    .from("deliveries as d")
+    .whereIn("match_id", subQuery)
+    .groupBy("bowler")
+    .orderBy("economy", "ASC")
+    .limit(10);
+
+  return mainQuery
     .then((result) => {
       return result;
     })
     .catch((err) => {
-      console.error(err);
+      throw err;
     })
     .finally(() => {
       knex.destroy();
